5f0ecaaa943d370dc0736ab26da251125c990aa2
Shivam kumar <sk2440038@gmail.com>
2026-05-31 16:58:17 +0530

    fix css path

--- Server02/server.js
+++ Server02/server.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, '..')));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('✅ MongoDB Connected'))
